20a76a1ef70069b36c9d72994aef40017532fc90
Reuben Thomas <reubenthomas.john2017@gmail.com>
2023-10-10 14:46:16 +0530

    some whitespace changes i suppose..

--- Endpoints/StudentsEndpoints.cs
+++ Endpoints/StudentsEndpoints.cs
@@ -1,3 +1,4 @@
+using Newtonsoft.Json;
 using StudentApi.Entities;
 using StudentApi.Repositories;
 
@@ -10,26 +11,28 @@ public static class StudentsEndpoints
     {
 
         var group = routes.MapGroup("/students");
-                            //.WithParameterValidation();
+        //.WithParameterValidation();
 
 
         group.MapGet("/", (IGamesRepository repository) => repository.GetAll());
 
-        group.MapGet("/{id}", (IGamesRepository repository, int id) => 
+        group.MapGet("/{id}", (IGamesRepository repository, int id) =>
         {
             Student? student = repository.Get(id);
-            return student is not null ? Results.Ok(student) : Results.NotFound();            
+            return student is not null ? Results.Ok(student) : Results.NotFound();
         }).WithName(GetStudentEndpointName);
 
-        group.MapPost("/", (IGamesRepository repository, Student student) => {
+        group.MapPost("/", (IGamesRepository repository, Student student) =>
+        {
             repository.Create(student);
-            return Results.CreatedAtRoute(GetStudentEndpointName, new {id = student.Id}, student);
+            return Results.CreatedAtRoute(GetStudentEndpointName, new { id = student.Id }, student);
         });
 
-        group.MapPut("/{id}", (IGamesRepository repository, int id, Student updatedStudent) => {
+        group.MapPut("/{id}", (IGamesRepository repository, int id, Student updatedStudent) =>
+        {
             Student? existingStudent = repository.Get(id);
 
-            if (existingStudent is null) 
+            if (existingStudent is null)
             {
                 return Results.NotFound();
             }
@@ -42,15 +45,16 @@ public static class StudentsEndpoints
             return Results.NoContent();
         });
 
-        group.MapDelete("/{id}", (IGamesRepository repository, int id) => {
+        group.MapDelete("/{id}", (IGamesRepository repository, int id) =>
+        {
             Student? student = repository.Get(id);
 
-            if (student is not null) 
+            if (student is not null)
             {
                 repository.Delete(id);
             }
             return Results.NoContent();
         });
         return group;
-    } 
+    }
 }
